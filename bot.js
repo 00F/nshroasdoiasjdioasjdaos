915eddc51e66fe52b74109b7a06877f82622373a
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-30 02:37:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,17 @@ console.log(".Online ");
  
 MeeRcY.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(` **-:heart: شوب عربي يحتاج دعمكم
--:rose: منتجات متميزة  
+  return channel.send(`
+**-:heart: شوب عربي يحتاج دعمكم
+-:rose: منتجات متميزة
 -:credit_card: تعامل ممتاز مع المشتريين
 -:earth_americas: قيف اوايات اسبوعية
 :gem: حياكم الله في سيرفركم المتواضع
-♯Black Shop :flag_black:***
+♯Black Shop :flag_black:**
 
-*[user]..**
 
-**https://discord.gg/9f3qvmh... [ ${member}  ]
-**`) 
+**http://discord.gg/9f3qvmh  [ ${member}  ]**
+`) 
 }).catch(console.error)
 })
 MeeRcY.login(process.env.NSHR);
